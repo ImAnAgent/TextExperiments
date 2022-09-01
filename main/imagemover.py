e58ce4844c33951846d0deec4be563093cfdd1d9
Maksud Gurbanli <gurbanlimaksud@gmail.com>
2022-09-01 15:39:49 +0200

    Add layers and adjust steps

--- main/imagemover.py
+++ main/imagemover.py
@@ -3,4 +3,4 @@ import splitfolders
 results_folder = 'smallresults/'
 
 splitfolders.ratio(results_folder, output=results_folder,
-    seed=1337, ratio=(.7, .15, .15), move=False)
+    seed=1337, ratio=(.8, .1, .1), move=False)
